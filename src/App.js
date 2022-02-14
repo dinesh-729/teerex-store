776d7a4a71e21dcf1ac7d9b5f1a3981ef7632794
Dinesh <dinesh729.dm@gmail.com>
2022-02-14 08:24:00 +0530

    routes setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
+
+import Home from './pages/home'
+import Cart from './pages/cart'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      Teerex Store
-    </div>
+    <Router>
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />} exact />
+          <Route path='/cart' element={<Cart />} exact />
+          <Route path="*" element={
+              <p>404: Page Not Found</p>
+            }
+          />
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
